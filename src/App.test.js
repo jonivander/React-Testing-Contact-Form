d9edb87e6f8c39df61447606d2d38d20066bdaca
jonathan warner <j.m.warners@gmail.com>
2020-08-06 14:34:28 -0700

    Got test set up, now fixing errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("Fills in and submits form", () => {
+  render(<App />);
+
+  const firstNameInput = screen.getByPlaceholderText(/first/i);
+  const lastNameInput = screen.getByPlaceholderText(/last/i);
+  const emailInput = screen.getByPlaceholderText(/email/i);
+  const messageInput = screen.getByPlaceholderText(/message/i);
+
+  fireEvent.change(firstNameInput, { target: { value: 'Jonathan' } } );
+  fireEvent.change(lastNameInput, { target: { value: 'Warner' } } );
+  fireEvent.change(emailInput, { target: { value: 'jonathan-warner@lambdastudents.com' } } );
+  fireEvent.change(messageInput, { target: { value: 'Hello World!' } } );
+
+  const submitBtn = screen.getByRole('button', { type: /submit/i} );
+
+  fireEvent.click(submitBtn);
+
+  expect(screen.getByText(/Jonathan/i)).toBeInTheDocument(); 
+}); 
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,9 +16,10 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id="firstName"
             name="firstName"
-            placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            placeholder="First"
+            ref={register({ required: true, maxLength: 15 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -28,8 +29,9 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
-            placeholder="Burke"
+            placeholder="Last"
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -38,24 +40,32 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label htmlFor="email">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input 
+            id="email" 
+            name="email" 
+            placeholder="Email"
+            ref={register({ required: true })} 
+          />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
-          <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <label id="message" htmlFor="message">Message</label>
+          <textarea 
+            name="message" 
+            placeholder="Message"
+            ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input id="submitBtn" type="submit" />
       </form>
     </div>
   );
